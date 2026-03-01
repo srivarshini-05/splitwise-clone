afef0b7785dde18f3ae9ec4916a524fe26a08efb
Srivarshini <psrivarshini80gmail.com>
2026-03-01 19:43:53 +0530

    Added gitignore and env setup

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root123",
-  database: "expense_tracker"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root123",
+  database: process.env.DB_NAME || "expense_tracker",
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
