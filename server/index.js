b1b606020b45ae574261d657717548085ea67a45
Srivarshini <psrivarshini80gmail.com>
2026-03-02 16:05:09 +0530

    fix render port and cors

--- server/index.js
+++ server/index.js
@@ -4,8 +4,14 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
+
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/groups", require("./routes/groups"));
 app.use("/api/overview", require("./routes/overview"));
@@ -17,6 +23,8 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
